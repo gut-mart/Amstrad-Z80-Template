22c3c5302554caa9fadfdc8a84d342517d4b08d5
gut-mart <pk2rx59es@gmail.com>
2025-08-31 14:45:47 +0200

    Cambios en plantilla_v1

---
+++ src/main.asm
@@ -0,0 +1,37 @@
+; ==========================================================
+; Programa para Amstrad CPC 6128
+; 
+; 
+;
+; Ensamblador: SJAsmPlus
+; ==========================================================
+
+    DEVICE AMSTRADCPC6128     ; Indica al ensamblador que es para un CPC 6128
+LOAD_ADRESS EQU $4000         ; Asigna a la variable LOAD_ADRESS la dirección de memoria.
+
+                             
+    ORG LOAD_ADRESS           ; Origen del programa en la memoria RAM
+	;************* Sección de Datos **************
+
+
+
+
+
+START:
+
+   ;***********sección de instrucciones**********
+   ;
+   ;
+   ;
+   ;
+   ;
+   ;
+   ;
+   ;
+    END START
+    
+
+	
+
+
+
