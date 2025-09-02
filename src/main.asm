9e1f54f203b1642678e62478d103c8b0aa618a72
gut-mart <pk2rx59es@gmail.com>
2025-09-02 19:37:51 +0200

    feat: Añade macro para gestionar la paleta de colores
    Se ha añadido una macro para gestionar la paleta de colores del Gate Array, y se ha cambiado la dirección de carga del programa a 000.

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,7 @@
              
  DEVICE AMSTRADCPC6128 ;; Especifica que se manejará la arquitectura
                        ;; para la computadora Amstrad CPC6128
-LOAD_ADDRESS EQU $4000 ;; Dirección donde se cargara el código en memoria.
+LOAD_ADDRESS EQU $8000 ;; Dirección donde se cargara el código en memoria.
                        ;; IMPORTANTE LA ETIQUETA "LOAD_ADDRESS" SIEMPRE DEBE 
                        ;; UTILIZARSE PARA INDICAR LA DIRECCION DE CARGA DEL 
                        ;; CODIGO DEL PROGRAMA EN LA MEMORIA. La dirección por 
@@ -16,13 +16,28 @@ LOAD_ADDRESS EQU $4000 ;; Dirección donde se cargara el código en memoria.
 
 
          
-START:
-;
-;
-;
-;
+      
+     
 
-        
+        MACRO   INK pen, col
+            ld      bc,$7F00
+            ld      a,$40 + pen
+            out     (c),a
+            ld      a,col         ; 0..26
+            out     (c),a
+        ENDM
+
+START:
+        di
+        INK 0, 20     ; papel verde
+        INK 1, 24     ; pluma 1 amarilla
+        INK 2, 6      ; pluma 2 azul
+        INK 16, 14    ; borde gris
+
+wait:   jr wait
+
+
+
         END START     ;;  END START indica cual es el punto de entrada.
                       ;;  IMPORTANTE LA ETIQUETA "START:" SIEMPRE DEBE 
                       ;;  UTILIZARSE PARA INDICAR LA DIRECCION DONDE DEBE COMENZAR EL
@@ -30,4 +45,4 @@ START:
                       ;;  mediante el  script create_dsk.sh que será  utilizado por
                       ;;  el programa iDSK para crear un archivo con estensión .dsk
 
-
+   
