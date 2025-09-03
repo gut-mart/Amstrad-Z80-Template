3339e44de2e7b3068765c774a44670a2a62b982c
gut-mart <pk2rx59es@gmail.com>
2025-09-03 19:34:35 +0200

    cambios

--- src/main.asm
+++ src/main.asm
@@ -19,23 +19,17 @@ LOAD_ADDRESS EQU $8000 ;; Dirección donde se cargara el código en memoria.
       
      
 
-        MACRO   INK pen, col
-            ld      bc,$7F00
-            ld      a,$40 + pen
-            out     (c),a
-            ld      a,col         ; 0..26
-            out     (c),a
-        ENDM
+     
 
 START:
-        di
-        INK 0, 20     ; papel verde
-        INK 1, 24     ; pluma 1 amarilla
-        INK 2, 6      ; pluma 2 azul
-        INK 16, 14    ; borde gris
-
-wait:   jr wait
-
+ ;
+ ;
+ ;
+ ;
+ ;
+ ;
+ ;
+ ;
 
 
         END START     ;;  END START indica cual es el punto de entrada.
