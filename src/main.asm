214222d36ea13f03d16ee4f33fba30530924ecda
gut-mart <pk2rx59es@gmail.com>
2025-09-06 23:34:51 +0200

    Configuración mínima para integrar  Dezog y MAME en VisualCode ver0.En el commit anterior se puede emular solo con WinAPE.

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,7 @@
              
  DEVICE AMSTRADCPC6128 ;; Especifica que se manejará la arquitectura
                        ;; para la computadora Amstrad CPC6128
+ SLDOPT COMMENT WPMEM, LOGPOINT, ASSERTION
 LOAD_ADDRESS EQU $4000 ;; Dirección donde se cargara el código en memoria.
                        ;; IMPORTANTE LA ETIQUETA "LOAD_ADDRESS" SIEMPRE DEBE 
                        ;; UTILIZARSE PARA INDICAR LA DIRECCION DE CARGA DEL 
@@ -40,3 +41,7 @@ START:
                       ;;  el programa iDSK para crear un archivo con estensión .dsk
 
    
+//En este punto se trata de integrar  Dezog y MAME para depurar el código ensamblador
+//para probar primero debes de lanzar mame desde la linea de comandos 
+// mame cpc6128 -flop1 "/home/isidro/Datos/Z80/Proyectos_ASM/Amstrad/Amstrad cpc-6128k/prg/build/disco.dsk" -debug -debugger gdbstub -debugger_port 12000
+ 
